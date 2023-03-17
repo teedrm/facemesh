976f306ac06def51f6e45b0ac72df2ae643cc67a
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-03-17 14:31:03 -0600

    set up face detection + getting landmarks from  facemesh

--- FaceMeshBasic.py
+++ FaceMeshBasic.py
@@ -4,8 +4,26 @@ import time
 
 cap = cv2.VideoCapture("video/1.mp4")
 previousTime = 0
+
+mpDraw = mp.solutions.drawing_utils
+mpFaceMesh = mp.solutions.face_mesh
+faceMesh = mpFaceMesh.FaceMesh(max_num_faces=2)
+
+#change thickness of line/circles
+
+
 while True:
     success, img = cap.read()
+    #find different points on face
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = faceMesh.process(imgRGB)
+    
+    #display results - when something is dectected -> draw
+    if results.multi_face_landmarks:
+        #loop through the faces if there's more than 1
+        for faceLms in results.multi_face_landmarks:
+            mpDraw.draw_landmarks(img, faceLms, mpFaceMesh.FACEMESH_CONTOURS)
+
     currentTime = time.time()
     fps = 1/(currentTime-previousTime)
     previousTime = currentTime
